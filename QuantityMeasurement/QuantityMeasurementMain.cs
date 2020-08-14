90a1254a3209fd5edc591d8be411fcf57a9f7562
karthik-karotra <karthikpatel54@gmail.com>
2020-08-14 10:03:29 +0530

    [Karthik]ADDED : Code & Test For Equality Check For Feet

--- QuantityMeasurement/QuantityMeasurementMain.cs
+++ QuantityMeasurement/QuantityMeasurementMain.cs
@@ -7,16 +7,50 @@ namespace QuantityMeasurement
     /// <summary>
     /// Quantity Measurement Class.
     /// </summary>
-    public static class QuantityMeasurementMain
+    public class QuantityMeasurementMain
     {
         /// <summary>
         /// method to get feet.
         /// </summary>
         /// <param name="feet">feet value to get converted.</param>
         /// <returns>feet.</returns>
-        public static double GetFeet(double feet)
+        public double GetFeet(double feet)
         {
             return feet;
+        }
+
+        /// <summary>
+        /// Equals method.
+        /// </summary>
+        /// <param name="obj">Object Of Quantity Measurement Main Class.</param>
+        /// <returns>Boolean Value According To Object.</returns>
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            if (obj == this)
+            {
+                return true;
+            }
+
+            if (obj is QuantityMeasurementMain)
+            {
+                return true;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// HashCode Of Object.
+        /// </summary>
+        /// <returns>Returns HashCode.</returns>
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
         }
     }
 }
--- QuantityMeasurementTest/QuantityMeasurementTest.cs
+++ QuantityMeasurementTest/QuantityMeasurementTest.cs
@@ -12,23 +12,78 @@ namespace QuantityMeasurementTest
     /// </summary>
     public class QuantityMeasurementTest
     {
+        private QuantityMeasurementMain quantityMeasurement;
+
         /// <summary>
         /// SetUp Method.
         /// </summary>
         [SetUp]
         public void Setup()
         {
+            this.quantityMeasurement = new QuantityMeasurementMain();
         }
 
         /// <summary>
-        /// Test Method.
+        /// Test method for checking equality between o feet and o feet.
         /// </summary>
         [Test]
-        public void Test1()
+        public void GivenZeroFeetAndZeroFeet_WhenEqual_ShouldReturnTrue()
         {
-            double feet1 = QuantityMeasurementMain.GetFeet(0.0);
-            double feet2 = QuantityMeasurementMain.GetFeet(0.0);
+            double feet1 = this.quantityMeasurement.GetFeet(0.0);
+            double feet2 = this.quantityMeasurement.GetFeet(0.0);
             Assert.AreEqual(feet1, feet2);
+        }
+
+        /// <summary>
+        /// Test method for checking equality between o feet and o feet.
+        /// </summary>
+        [Test]
+        public void GivenNullFeetValue_ShouldReturnFalse()
+        {
+            Assert.IsFalse(this.quantityMeasurement.Equals(null));
+        }
+
+        /// <summary>
+        /// Test method for checking class reference.
+        /// </summary>
+        [Test]
+        public void GivenClassReference_WhenChecked_ShouldReturnTrue()
+        {
+            bool result = this.quantityMeasurement.Equals(this.quantityMeasurement);
+            Assert.IsTrue(result);
+        }
+
+        /// <summary>
+        /// Test method for checking type.
+        /// </summary>
+        [Test]
+        public void GivenClassReference_WhenTypeChecked_ShouldReturnTrue()
+        {
+            QuantityMeasurementMain quantityMeasurement1 = new QuantityMeasurementMain();
+            QuantityMeasurementMain quantityMeasurement2 = new QuantityMeasurementMain();
+            Assert.AreEqual(quantityMeasurement1, quantityMeasurement2);
+        }
+
+        /// <summary>
+        /// Test method for equality check of 2 same feet values.
+        /// </summary>
+        [Test]
+        public void GivenTwoSameFeetValues_WhenEqual_ShouldReturnTrue()
+        {
+            double feet1 = this.quantityMeasurement.GetFeet(1.0);
+            double feet2 = this.quantityMeasurement.GetFeet(1.0);
+            Assert.AreEqual(feet1, feet2);
+        }
+
+        /// <summary>
+        /// Test method for equality check of 2 different feet values.
+        /// </summary>
+        [Test]
+        public void GivenTwoDifferentFeetValues_WhenEqual_ShouldReturnFalse()
+        {
+            double feet1 = this.quantityMeasurement.GetFeet(1.0);
+            double feet2 = this.quantityMeasurement.GetFeet(10.0);
+            Assert.AreNotEqual(feet1, feet2);
         }
     }
 }
