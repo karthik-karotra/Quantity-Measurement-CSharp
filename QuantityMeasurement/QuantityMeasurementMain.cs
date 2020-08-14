118cd66361a4520ce735d17608005f975142e6ca
karthik-karotra <karthikpatel54@gmail.com>
2020-08-14 08:20:10 +0530

    [Karthik]ADDED : Empty Library And NUnit Test Project

---
+++ QuantityMeasurement/QuantityMeasurementMain.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="QuantityMeasurementMain.cs" company="BridgeLabz Solutions">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace QuantityMeasurement
+{
+    /// <summary>
+    /// Quantity Measurement Class.
+    /// </summary>
+    public class QuantityMeasurementMain
+    {
+    }
+}
---
+++ QuantityMeasurementTest/QuantityMeasurementTest.cs
@@ -0,0 +1,31 @@
+﻿// <copyright file="QuantityMeasurementTest.cs" company="BridgeLabz Solutions">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace QuantityMeasurementTest
+{
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Quantity Measurement Class.
+    /// </summary>
+    public class QuantityMeasurementTest
+    {
+        /// <summary>
+        /// SetUp Method.
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        /// <summary>
+        /// Test Method.
+        /// </summary>
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
